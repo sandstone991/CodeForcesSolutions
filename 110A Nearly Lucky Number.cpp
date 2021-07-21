f2f648ae086b45d3961aa5a18b1cc917f94b7857
Ahmed Mahmoud Azzam <gamerazzam@gmail.com>
2021-07-21 15:36:07 +0200

    commit

---
+++ 110A Nearly Lucky Number.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+typedef long long ll;
+int main()
+{
+    ll baseNumber;
+    int temp;
+    int cntr = 0;
+    cin >> baseNumber;
+    while (baseNumber != 0)
+    {
+        if ((temp = baseNumber % 10) == 4 || temp == 7)
+        {
+            cntr++;
+        }
+        baseNumber -= temp;
+        baseNumber /= 10;
+    }
+    if (cntr == 4 || cntr == 7)
+    {
+        cout << "YES";
+        return 0;
+    }
+    cout << "NO";
+}
